cf0ce65da0f896b754807e07f78f409f7941812b
Elenhtml <bagslife@yandex.ru>
2018-05-05 00:37:10 +0300

    Done task 24

--- js/backend.js
+++ js/backend.js
@@ -1,67 +1,34 @@
 'use strict';
 
 (function () {
-  /* var xhr = new XMLHttpRequest();
-  var commonFunction = function () {
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
-    });
-  }; */
-  var loadData = function (onLoad, onError) {
-    var URL = 'https://js.dump.academy/keksobooking/data';
-    // commonFunction();
+  var SUCCESS_STATUS_CODE = 200;
+  var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+  var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
+
+  var getServerConnection = function (onLoad, onError, method, url, data) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
+      if (xhr.status === SUCCESS_STATUS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
     });
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения');
-      var errorHandler = function (errorMessage) {
-        var showMessage = document.createElement('div');
-        showMessage.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-        showMessage.style.position = 'absolute';
-        showMessage.style.left = 0;
-        showMessage.style.right = 0;
-        showMessage.style.fontSize = '30px';
-        showMessage.textContent = errorMessage;
-        document.body.insertAdjacentElement('afterbegin', showMessage);
-        setTimeout(function () {
-          showMessage.parentNode.removeChild(showMessage);
-        }, 3000);
-      };
-      loadData(errorHandler);
     });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-    xhr.timeout = 1000;
-    xhr.open('GET', URL);
-    xhr.send();
-  };
 
-  var sendInfoToServer = function (data, onLoad, onError) {
-    var URL = 'https://js.dump.academy/keksobooking';
-    // commonFunction();
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
-    });
-    xhr.addEventListener('error', function () {
-      if (onError('Произошла ошибка соединения')) {
-        onError('Произошла ошибка соединения');
-      } else if (onError(xhr.status + ' ' + xhr.statusText)) {
-        onError(xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.open('POST', URL);
+    xhr.open(method, url);
     xhr.send(data);
   };
+
   window.backend = {
-    // commonFunction: commonFunction,
-    loadData: loadData,
-    sendInfoToServer: sendInfoToServer
+    loadData: function (onLoad, onError) {
+      getServerConnection(onLoad, onError, 'GET', LOAD_URL);
+    },
+    uploadData: function (onLoad, onError, data) {
+      getServerConnection(onLoad, onError, 'POST', UPLOAD_URL, data);
+    }
   };
 })();
--- js/pins.js
+++ js/pins.js
@@ -11,15 +11,17 @@
     HEIGHT: 40,
     CORNER: 22
   };
-
+  var DEFAULT_OFFERS = 5;
+  var adressesNew = [];
+  
   var similarListElement = document.querySelector('.map__pins');
   var similarMapPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
   var createMapPin = function (mapPin) {
     var mapPinElement = similarMapPinTemplate.cloneNode(true);
     mapPinElement.style = 'left: ' + (mapPin.location.x - window.data.MAP_PIN_WIDTH / 2) + 'px;' + 'top: ' + (mapPin.location.y - window.data.MAP_PIN_HEIGHT) + 'px;';
-    mapPinElement.querySelector('img').src = mapPin.author;
-    mapPinElement.querySelector('img').alt = mapPin.title;
+    mapPinElement.querySelector('img').src = mapPin.author.avatar;
+    mapPinElement.querySelector('img').alt = mapPin.offer.title;
 
     mapPinElement.addEventListener('click', function () {
       window.card.closePopup();
@@ -30,6 +32,7 @@
   };
 
   var fragment = document.createDocumentFragment();
+  
   var setAllElements = function (arr) {
     for (var i = 0; i < arr.length; i++) {
       fragment.appendChild(createMapPin(arr[i]));
@@ -39,7 +42,28 @@
 
   adFormAddress.value = MAP_PIN_MAIN_COORDINATES.LEFT + ', ' + MAP_PIN_MAIN_COORDINATES.TOP;
 
-  // var adressesNew = [];
+  var onError = function (message) {
+    var showMessage = document.createElement('div');
+    showMessage.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    showMessage.style.position = 'absolute';
+    showMessage.style.left = 0;
+    showMessage.style.right = 0;
+    showMessage.style.fontSize = '30px';
+    showMessage.textContent = message;
+    document.body.insertAdjacentElement('afterbegin', showMessage);
+    setTimeout(function () {
+      showMessage.parentNode.removeChild(showMessage);
+    }, 3000);
+  };
+    
+  var setDefaultAddress = function () {
+    adFormAddress.value = MAP_PIN_MAIN_COORDINATES.LEFT + ', ' + MAP_PIN_MAIN_COORDINATES.TOP;
+  };
+  
+  var setAddress = function () {
+    adFormAddress.value = (MAP_PIN_MAIN_COORDINATES.LEFT + window.data.MAP_PIN_WIDTH / 2) + ', ' + (MAP_PIN_MAIN_COORDINATES.TOP + MAP_PIN_MAIN_COORDINATES.HEIGHT + MAP_PIN_MAIN_COORDINATES.CORNER);
+  };
+  
   mainPin.addEventListener('mouseup', function () {
     mapShow.classList.remove('map--faded');
     window.form.adForm.classList.remove('ad-form--disabled');
@@ -47,14 +71,12 @@
       fieldsets[i].removeAttribute('disabled');
     }
 
-    adFormAddress.value = (MAP_PIN_MAIN_COORDINATES.LEFT + window.data.MAP_PIN_WIDTH / 2) + ', ' + (MAP_PIN_MAIN_COORDINATES.TOP + MAP_PIN_MAIN_COORDINATES.HEIGHT + MAP_PIN_MAIN_COORDINATES.CORNER);
+    setAddress();
 
-    setAllElements(window.data.massAdds);
-    /* window.backend.loadData(function (res) {
-      adressesNew = res;
-      setAllElements(adressesNew);
-    }, onError); */
-    similarListElement.appendChild(fragment);
+    window.backend.loadData(function (res) {
+      adressesNew = res.slice(DEFAULT_OFFERS);
+      similarListElement.appendChild(setAllElements(adressesNew));
+    }, onError);
   });
 
   var MapCoords = {
@@ -124,6 +146,7 @@
     mapShow: mapShow,
     mainPin: mainPin,
     fieldsets: fieldsets,
+    setDefaultAddress: setDefaultAddress,
     adFormAddress: adFormAddress
   };
 })();
